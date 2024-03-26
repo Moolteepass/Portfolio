d60cf6ba6264b39da2cdd07e1d5bc71597b73231
Quabmac <mccormack.jacob@gmail.com>
2024-03-26 14:45:17 +1100

    Really good state with carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 import HomePage from "./components/HomePage"
 
 const App = () => {
+  const [showNavbarBg, setShowNavbarBg] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY
+      if (scrollPosition > 700) {
+        setShowNavbarBg(true)
+      } else {
+        setShowNavbarBg(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
   return (
     <Router>
       <div className="Full-App">
         <aside className="Nav-Main">
-          <div className="Nav-Container">
+          <div
+            className={
+              showNavbarBg
+                ? "Nav-Container Nav-With-Background"
+                : "Nav-Container"
+            }
+          >
             <img
               className="Nav-Logo"
               src="https://www.dropbox.com/scl/fi/q4pgpe5ukia0q6v4oqh3n/MonkeyMedia.png?rlkey=3zf7zuxrfqn2r5vogvy1fqazq&raw=1"
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,8 +2,12 @@ import Carousel from "./Carousel"
 
 const HomePage = () => {
   return (
-    <div className="HomePage-Main">
-      <Carousel />
+    <div className="HomePage">
+      <div className="HomePage-Main">
+        <Carousel />
+      </div>
+      <h1 className="What-I-Do">THE KIND OF WORK I DO</h1>
+      <div></div>
     </div>
   )
 }
