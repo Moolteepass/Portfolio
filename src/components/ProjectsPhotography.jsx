c806a2377452224719e43d01e2f2c45b0da510b4
Quabmac <mccormack.jacob@gmail.com>
2024-04-04 19:44:45 +1100

    masonry

--- src/components/ProjectsPhotography.jsx
+++ src/components/ProjectsPhotography.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react"
 import AnimatedPage from "./AnimatedPage"
 
+import { Masonry } from "@mui/lab"
+
 const ProjectsPhotography = () => {
+  const [randomNumbers, setRandomNumbers] = useState([])
+
+  useEffect(() => {
+    const generateRandomNumbers = (max) => {
+      const numbers = []
+      while (numbers.length < max) {
+        const randomNumber = Math.floor(Math.random() * max) + 1
+        if (!numbers.includes(randomNumber)) {
+          numbers.push(randomNumber)
+        }
+      }
+      return numbers
+    }
+
+    // Generate an array of 67 unique random numbers
+    setRandomNumbers(generateRandomNumbers(134))
+  }, [setRandomNumbers])
+
   return (
     <AnimatedPage>
-      <div>ProjectsPhotography</div>
+      <div className="Photography-All">
+        <Masonry columns={3} spacing={1}>
+          {randomNumbers.map((number, index) => (
+            <img
+              loading="lazy"
+              src={`https://monkey-media-portfolio-images.s3.ap-southeast-2.amazonaws.com/photography/fashion/${number}.jpg`}
+              key={index}
+            ></img>
+          ))}
+        </Masonry>
+      </div>
     </AnimatedPage>
   )
 }
