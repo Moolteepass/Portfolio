0f2faa748a382482f62ca30d81c727538f3c2fde
Quabmac <97374115+Moolteepass@users.noreply.github.com>
2025-07-10 15:29:32 +1000

    new

--- app/loading.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="loading-spinner">
-      <div className="spinner"></div>
-    </div>
-  )
-}
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.amazonaws.com",
-      },
-    ],
-  },
-}
+const nextConfig = {};
 
-export default nextConfig
+export default nextConfig;
